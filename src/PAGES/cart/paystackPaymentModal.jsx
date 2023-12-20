fb98c3813b8a47801474e03125e394078417399f
setongee <setongee@gmail.com>
2023-12-20 13:10:30 +0100

    reoved red bar

--- src/PAGES/cart/paystackPaymentModal.jsx
+++ src/PAGES/cart/paystackPaymentModal.jsx
@@ -8,20 +8,25 @@ import { addSuccessfulOrder } from '../../apis/shop';
 
 export default function PaystackPaymentModal({shipping, price, delivery, closePayment, cart}) {
 
+    const live_key = 'pk_live_99d322807edd18dffe32153e5c6fdff623665876';
+    const test_key = "pk_test_3bf17d5bf39878cc53b53a151648c3d404936307"
+
     let navigate = useNavigate()
+
+    const amount = price + delivery;
 
     const config = {
 
         reference: (new Date()).getTime().toString(),
         email: shipping.email,
-        amount: (price + delivery) * 100,
-        publicKey: 'pk_live_99d322807edd18dffe32153e5c6fdff623665876',
+        amount: amount * 100,
+        publicKey: live_key,
 
     };
 
     const onSuccess = (reference) => {
 
-        addSuccessfulOrder({products : cart, ref : reference, shipping : shipping })
+        addSuccessfulOrder({products : cart, ref : reference, shipping : shipping, totalAmount : amount })
         .then( () => {
 
             clearCart();
@@ -48,7 +53,7 @@ export default function PaystackPaymentModal({shipping, price, delivery, closePa
                 <div className="checkoutpay">
 
                     <div className="logoCart"> <img src={mercLogo} alt="cart images" /> </div>
-                    <p>Hey Setonji, you are about to make a payment of</p>
+                    <p>Hey <strong>{shipping.firstname}</strong>, you are about to make a payment of</p>
                     <h1>N{(price + delivery).toLocaleString()}.00</h1>
                     <div className="h1Line"></div>
                     <p>By clicking and continueing the button below, you will be redirected to our payment portal to initiate payment via different payment options.</p>
